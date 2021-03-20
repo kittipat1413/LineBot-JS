f33ab9b82b3455bbfbd59a1ca2150904480faeb1
kittipat <kittipat_1413@hotmail.com>
2021-03-21 00:05:12 +0700

    add test path

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
